539e476a91b334cb806c8feabf550cf8deec9833
Jed <jamijares3@gmail.com>
2019-07-20 10:53:16 -0400

    adds error checking and invert option

--- source/main.cpp
+++ source/main.cpp
@@ -12,23 +12,46 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    std::string filename = argv[1];
-    Magick::Image inputImg(filename);
-
-    for(int y = 0; y < inputImg.rows(); y++)
+    bool invertFlag = false;
+    int argvIndex = 1;
+    while ((argvIndex < argc - 1) && (*argv[argvIndex] == '-'))
     {
-        for(int x = 0; x < inputImg.columns(); x++)
+        std::string argument(argv[argvIndex]);
+        if (argument.compare("-i") == 0)
         {
-            for(int i = 0; i < 3; i++) // print each character 3 times to account for difference in length to width
+            invertFlag = true;
+            argvIndex++;
+        }
+        
+    }
+
+    try
+    {
+        std::string filename = argv[argc - 1];
+        Magick::Image inputImg(filename);
+        // inputImg.zoom(Magick::Geometry(inputImg.columns()/7,inputImg.rows()/7));
+    
+        for(int y = 0; y < inputImg.rows(); y++)
+        {
+            for(int x = 0; x < inputImg.columns(); x++)
             {
                 // https://en.wikipedia.org/wiki/Relative_luminance
                 float pixelBrightness = 0.2126*(inputImg.pixelColor(x,y)).quantumRed() + 0.7152*(inputImg.pixelColor(x,y)).quantumGreen() + 0.0722*(inputImg.pixelColor(x,y)).quantumBlue();
-                const std::string asciiChart = " `^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+                const char* asciiChart = " `^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"; // ASCII from dimmest to brightest
                 int charIndex = (pixelBrightness)/QuantumRange * sizeof(asciiChart); // map brightness value to a printable character in array
-                std::cout << asciiChart[charIndex];
+                for(int i = 0; i < 2; i++) // print each character multiple times to account for difference in length to width
+                {
+                    if(!invertFlag) std::cout << asciiChart[charIndex];
+                    else std::cout << asciiChart[sizeof(asciiChart) - charIndex];
+                }
             }
+            std::cout << "\n";
         }
-        std::cout << "\n";
+    }
+    catch( std::exception& error_ )
+    {
+      std::cout << "Caught exception: " << error_.what() << "\n";
+      return 1;
     }
 
     return 0;
